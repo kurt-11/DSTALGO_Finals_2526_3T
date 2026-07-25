d71e3055d17234da80486322b368be900aad1723
kurt-11 <kurtjhervin09@gmail.com>
2026-07-25 23:17:20 +0800

    Implement ShoppingCart.cs

--- ECommerceSystem/ECommerceSystem.Core/ShoppingCart.cs
+++ ECommerceSystem/ECommerceSystem.Core/ShoppingCart.cs
@@ -6,15 +6,49 @@ public class ShoppingCart
 {
     private readonly CustomArrayList<Product> _items = new();
 
+    // Returns the total number of products in the shopping cart.
     public int Count => _items.Count;
 
-    public void AddItem(Product product) => throw new NotImplementedException();
-    public bool RemoveItem(Product product) => throw new NotImplementedException();
-    public Product GetItemAt(int index) => throw new NotImplementedException();
+    // Adds a product to the shopping cart.
+    public void AddItem(Product product)
+    {
+        _items.Add(product);
+    }
 
-    public decimal CalculateTotal() => throw new NotImplementedException();
+    // Removes a product from the shopping cart.
+    public bool RemoveItem(Product product)
+    {
+        return _items.Remove(product);
+    }
 
-    
-    public int SearchItem(Product product) => throw new NotImplementedException();
-    public void SortCartByPrice() => throw new NotImplementedException();
+    // Returns the product at the specified index.
+    public Product GetItemAt(int index)
+    {
+        return _items.Get(index);
+    }
+
+    // Calculates the total price of all products in the shopping cart.
+    public decimal CalculateTotal()
+    {
+        decimal total = 0;
+
+        for (int i = 0; i < _items.Count; i++)
+        {
+            total += _items.Get(i).Price;
+        }
+
+        return total;
+    }
+
+    // Searches for a product and returns its index.
+    public int SearchItem(Product product)
+    {
+        return _items.Search(product);
+    }
+
+    // Sorts the products in the shopping cart by price.
+    public void SortCartByPrice()
+    {
+        _items.Sort();
+    }
 }
